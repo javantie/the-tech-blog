0cb91b2a30b0b4a532638c4e89abe3b1b342b9d4
Akeem C <javantieclarke@gmail.com>
2021-09-12 12:10:19 -0400

    updated the view for sigle post and comments

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -2,7 +2,7 @@ async function editFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value.trim();
-  const post_url = document.querySelector('textarea[name="post-body"]').value.trim();
+  const post_url = document.querySelector('textarea[name="post-url"]').value.trim();
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
